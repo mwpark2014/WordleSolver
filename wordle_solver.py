b078d7a81f5dba58420da227bbf639d6fd01ff64
Mason <park.mason.w@gmail.com>
2022-01-30 22:01:48 -0800

    Wordle_solver class: naive impl Filter words with word_length length and get_best_freq_score_word, create_freq_dict

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,10 +1,12 @@
 import argparse
+import twl
+import heapq
 
 parser = argparse.ArgumentParser(description='A Wordle puzzle solver.')
 parser.add_argument("-w", "--word_length", type=int, help="Set word length", default=5)
 parser.add_argument("-n", "--num_attempts", type=int, help="Set number of attempts", default=6)
 
-
+# The Wordle class defining a puzzle, console UI, and a basic way to get input from the user
 class Wordle:
     CHAR_RESULT = {
         'NOT_CONTAINED': 0,
@@ -12,8 +14,7 @@ class Wordle:
         'CORRECT': 2,
     }
 
-    # If answer is none, this Wordle does not have insight to the answer
-    # and needs outside help from the user to determine how correct attempts are
+    # Defaults are defined in the ArgumentParser
     def __init__(self, word_length, num_attempts):
         self.word_length = word_length
         self.num_attempts = num_attempts
@@ -74,6 +75,8 @@ class Wordle:
             self.get_automated_attempt_response(self.attempts[-1], answer)
         return False
 
+
+
     def _validate_word(self, word):
         if not word:
             raise ValueError('Invalid None value for response')
@@ -97,9 +100,48 @@ class Wordle:
         print()
 
 
-# class WordleSolver:
+# The automated solver that will solve a given Wordle
+class WordleSolver:
+    def __init__(self, word_length, num_attempts):
+        self.word_length = word_length
+        self.num_attempts = num_attempts
+        self.filtered_words_by_length = set(filter(lambda word: len(word) == word_length, set(twl.iterator())))
+        print("{} potential words".format(len(self.filtered_words_by_length)))
+
+    def create_freq_dict(self):
+        freq_dict = {}
+        for word in self.filtered_words_by_length:
+            for i in range(len(word)):
+                key = self._get_freq_dict_key(word, i)
+                freq_dict[key] = freq_dict.get(key, 0) + 1
+        return freq_dict
+
+    def get_best_freq_score_word(self, freq_dict):
+        freq_word_tuple = self.create_word_freq_score_heap(freq_dict)[0]
+        return freq_word_tuple[1]
+
+    def create_word_freq_score_heap(self, freq_dict):
+        freq_word_tuples = []
+        for word in self.filtered_words_by_length:
+            freq_score = 0
+            for i in range(len(word)):
+                key = self._get_freq_dict_key(word, i)
+                freq_score += freq_dict[key]
+            freq_word_tuples.append((-freq_score, word))
+        heapq.heapify(freq_word_tuples)
+        return freq_word_tuples
+
+    def _get_freq_dict_key(self, word, i):
+        return word[i] + str(i)
+
+    def solve(self) -> bool:
+        wordle = Wordle(self.word_length, self.num_attempts)
+        freq_dict = self.create_freq_dict()
+        next_word = self.get_best_freq_score_word(freq_dict)
+        print(next_word)
+        return False
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    wordle = Wordle(args.word_length, args.num_attempts, 'BULLY')
-    wordle.play_wordle_alone_with_answer()
+    wordle_solver = WordleSolver(args.word_length, args.num_attempts)
+    wordle_solver.solve()
--- wordle_solver_test.py
+++ wordle_solver_test.py
@@ -5,6 +5,7 @@ import wordle_solver
 class TestWordleSolverMethods(unittest.TestCase):
     def setUp(self) -> None:
         self.wordle = wordle_solver.Wordle(5, 6)
+        self.wordle_solver = wordle_solver.WordleSolver(5, 6)
 
     def test_make_attempt(self):
         self.wordle.make_attempt("point")
@@ -32,3 +33,26 @@ class TestWordleSolverMethods(unittest.TestCase):
         wordle.make_attempt_with_input = Mock(side_effect=lambda: wordle.make_attempt('POINT'))
         with self.assertRaises(ValueError):
             wordle.play_wordle_alone_with_answer(None)
+
+    def test_wordle_solver_create_freq_dict(self):
+        freq_dict = self.wordle_solver.create_freq_dict()
+        self.assertEqual(len(freq_dict), 129)
+        self.assertEqual(freq_dict['e2'], 580)
+        self.assertEqual(freq_dict['s4'], 2785)
+        with self.assertRaises(KeyError):
+            test_freq = freq_dict['s5']
+
+    def test_wordle_solver_create_word_freq_score_heap(self):
+        freq_dict = self.wordle_solver.create_freq_dict()
+        freq_score_heap = self.wordle_solver.create_word_freq_score_heap(freq_dict)
+        self.assertEqual(len(freq_score_heap), 8938)
+        self.assertEqual(freq_score_heap[0], (-7824, 'sores'))
+        self.assertEqual(freq_score_heap[1], (-7757, 'sales'))
+
+    def test_wordle_solver_get_best_freq_score_word(self):
+        freq_dict = self.wordle_solver.create_freq_dict()
+        best_freq_score_word = self.wordle_solver.get_best_freq_score_word(freq_dict)
+        self.assertEqual(best_freq_score_word, 'sores')
+
+    def test_wordle_solver_solve(self):
+        self.assertTrue(self.wordle_solver.solve(self.wordle))
